2111e35e4c5c343212c29634bd4aa436928db60c
SECT <info@sectwebstudio.com>
2020-03-20 03:09:08 +0900

    Migrate dev environment to webpack

---
+++ src/assets/js/_modules/commons.js
@@ -0,0 +1,12 @@
+// import 'picturefill';
+// import 'lazysizes/plugins/aspectratio/ls.aspectratio';
+// import 'lazysizes/plugins/bgset/ls.bgset';
+// import 'lazysizes/plugins/unveilhooks/ls.unveilhooks';
+// import 'lazysizes/plugins/object-fit/ls.object-fit';
+// import 'lazysizes/plugins/respimg/ls.respimg';
+// import 'lazysizes';
+import 'jquery.easing';
+// import viewportUnitsBuggyfill from 'viewport-units-buggyfill';
+import { detection as detect } from './detect';
+
+window.detect = detect();
---
+++ src/assets/js/_modules/select.js
@@ -0,0 +1,5 @@
+import 'select2';
+
+export const select = () => {
+  jQuery('#google_ss2db_dataformat').select2();
+};
