b2ab2e1c14de6f877af6e1669dce65421bdb3f54
SECT <info@sectwebstudio.com>
2020-03-20 09:45:55 +0900

    Rewrite JavaScript with TypeScript

--- src/assets/ts/_modules/delete-data.ts
+++ src/assets/ts/_modules/delete-data.ts
@@ -1,15 +1,27 @@
 import Swal from 'sweetalert2';
 
+declare global {
+  interface Window {
+    google_ss2db_data: GoogleSS2dbData;
+  }
+}
+declare let jQuery: any;
+
+interface GoogleSS2dbData {
+  nonce: string;
+  plugin_dir_url: string;
+}
+
 export const deleteData = () => {
-  jQuery('.acorddion .ss2db_delete').on('click', function() {
-    const theid = jQuery(this)
+  jQuery('.acorddion .ss2db_delete').on('click', (e: { currentTarget: HTMLElement }) => {
+    const theid = jQuery(e.currentTarget)
       .closest('dl')
       .attr('data-id');
     Swal.queue([
       {
         title: 'Are you sure?',
         text: "You won't be able to revert this!",
-        type: 'warning',
+        icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
@@ -20,9 +32,9 @@ export const deleteData = () => {
           return new Promise(resolve => {
             jQuery
               .ajax({
-                url: `${google_ss2db_data.plugin_dir_url}includes/delete.php`,
+                url: `${window.google_ss2db_data.plugin_dir_url}includes/delete.php`,
                 dataType: 'json',
-                data: { id: theid, nonce: google_ss2db_data.nonce },
+                data: { id: theid, nonce: window.google_ss2db_data.nonce },
                 type: 'post',
                 // beforeSend() {},
               })
@@ -38,7 +50,7 @@ export const deleteData = () => {
                   title: 'Something went wrong!',
                 });
               })
-              .always(data => {
+              .always((data: { res: number; id: string }) => {
                 if (data.res === 1) {
                   const ele = jQuery(`.acorddion[data-id='${data.id}']`);
                   jQuery
