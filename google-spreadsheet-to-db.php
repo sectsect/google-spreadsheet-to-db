5ccd427e55c65ffd12a25e6155eed34431341908
SECT <info@sectwebstudio.com>
2021-03-08 18:17:52 +0900

    Bump version v2.6.0

--- google-spreadsheet-to-db.php
+++ google-spreadsheet-to-db.php
@@ -7,7 +7,7 @@
  * Author URI:      https://www.ilovesect.com/
  * Text Domain:     google-spreadsheet-to-db
  * Domain Path:     /languages
- * Version:         2.5.3
+ * Version:         2.6.0
  *
  * @package         Google_Spreadsheet_to_DB
  */
