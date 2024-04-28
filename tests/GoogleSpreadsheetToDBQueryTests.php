ede890b9dd57ef4cba9ed492d3583beec88129b9
sect <sect@sect.me>
2024-04-29 03:18:29 +0900

    test: fix tests

--- tests/GoogleSpreadsheetToDBQueryTests.php
+++ tests/GoogleSpreadsheetToDBQueryTests.php
@@ -5,237 +5,155 @@ require_once __DIR__ . '/../includes/class-google-spreadsheet-to-db-query.php';
  * Class Test_Google_Spreadsheet_To_DB_Query
  * Tests the Google_Spreadsheet_To_DB_Query class functionality.
  */
-class Test_Google_Spreadsheet_To_DB_Query extends WP_UnitTestCase {
-	/**
-	 * @var Google_Spreadsheet_To_DB_Query Instance of Google_Spreadsheet_To_DB_Query to test.
-	 */
-	private $google_spreadsheet_to_db_query;
+class Google_Spreadsheet_To_DB_Query_Test extends WP_UnitTestCase {
 
-	/**
-	 * Sets up the environment for each test.
-	 * Mocks the global $wpdb and prepares a mock response for Google Sheets API.
-	 */
 	public function setUp(): void {
 		parent::setUp();
-
 		// Mock response from Google Sheets API.
-		$mock_api_response = array(
-			(object) array(
+		$this->mock_data = array(
+			array(
 				'id'             => 1,
-				'date'           => '2021-01-01',
-				'title'          => 'Sample Data 1',
-				'worksheet_name' => 'Sheet1',
+				'date'           => '2023-06-01 10:00:00',
+				'worksheet_id'   => 'sheet1',
+				'worksheet_name' => 'Sheet 1',
+				'sheet_name'     => 'Data 1',
+				'title'          => 'Title 1',
+				'value'          => '{"key1":"value1"}',
 			),
-			(object) array(
+			array(
 				'id'             => 2,
-				'date'           => '2021-01-02',
-				'title'          => 'Sample Data 2',
-				'worksheet_name' => 'Sheet1',
+				'date'           => '2023-06-02 11:00:00',
+				'worksheet_id'   => 'sheet1',
+				'worksheet_name' => 'Sheet 1',
+				'sheet_name'     => 'Data 2',
+				'title'          => 'Title 2',
+				'value'          => '{"key2":"value2"}',
 			),
-			(object) array(
+			array(
 				'id'             => 3,
-				'date'           => '2023-12-31',
-				'title'          => 'Sample Data 3',
-				'worksheet_name' => 'My Spreadsheet',
+				'date'           => '2023-06-03 12:00:00',
+				'worksheet_id'   => 'sheet2',
+				'worksheet_name' => 'Sheet 2',
+				'sheet_name'     => 'Data 3',
+				'title'          => 'Title 3',
+				'value'          => '{"key3":"value3"}',
 			),
-			(object) array(
+			array(
 				'id'             => 4,
-				'date'           => '2024-01-01',
-				'title'          => 'Sample Data 4',
-				'worksheet_name' => 'My Spreadsheet',
+				'date'           => '2023-06-04 13:00:00',
+				'worksheet_id'   => 'sheet2',
+				'worksheet_name' => 'Sheet 2',
+				'sheet_name'     => 'Data 4',
+				'title'          => 'Title 4',
+				'value'          => '{"key4":"value4"}',
 			),
-			(object) array(
-				'id'             => 5,
-				'date'           => '2024-01-02',
-				'title'          => 'Sample Data 5',
-				'worksheet_name' => 'My Spreadsheet',
-			),
-			(object) array(
-				'id'             => 6,
-				'date'           => '2024-01-03',
-				'title'          => 'Sample Data 6',
-				'worksheet_name' => 'Sheet2',
-			),
-		);
-
-		// Mocking wpdb.
-		global $wpdb;
-		$wpdb = $this->getMockBuilder( stdClass::class )
-					->setMethods( array( 'prepare', 'get_results', 'query' ) )
-					->getMock();
-
-		// Adding properties to mock object to avoid undefined property warnings
-		$wpdb->posts              = 'wp_posts';
-		$wpdb->postmeta           = 'wp_postmeta';
-		$wpdb->comments           = 'wp_comments';
-		$wpdb->commentmeta        = 'wp_commentmeta';
-		$wpdb->terms              = 'wp_terms';
-		$wpdb->term_taxonomy      = 'wp_term_taxonomy';
-		$wpdb->term_relationships = 'wp_term_relationships';
-		$wpdb->termmeta           = 'wp_termmeta';
-		$wpdb->users              = 'wp_users';
-		$wpdb->usermeta           = 'wp_usermeta';
-
-		$wpdb->method( 'prepare' )->will(
-			$this->returnCallback(
-				function ( $query, ...$params ) {
-					return $query;
-				}
-			)
-		);
-		$wpdb->method( 'get_results' )->will(
-			$this->returnCallback(
-				function ( $query ) use ( $mock_api_response ) {
-					// IDに基づくフィルタリング
-					if ( strpos( $query, 'WHERE id =' ) !== false ) {
-						$id     = intval( str_replace( 'SELECT * FROM data WHERE id =', '', $query ) );
-						$result = array_filter(
-							$mock_api_response,
-							function ( $item ) use ( $id ) {
-								return $item->id === $id;
-							}
-						);
-						return $result;
-					}
-					// オフセットと制限に基づくフィルタリング
-					if ( strpos( $query, 'LIMIT' ) !== false ) {
-						$limit  = intval( explode( ',', explode( 'LIMIT', $query )[1] )[1] );
-						$offset = intval( explode( ',', explode( 'LIMIT', $query )[1] )[0] );
-						return array_slice( $mock_api_response, $offset, $limit );
-					}
-					// ワークシート名に基づくフィルタリング
-					if ( strpos( $query, 'WHERE worksheet_name =' ) !== false ) {
-						$worksheet_name = str_replace( array( "SELECT * FROM data WHERE worksheet_name = '", "'" ), '', $query );
-						return array_filter(
-							$mock_api_response,
-							function ( $item ) use ( $worksheet_name ) {
-								return $item->worksheet_name === $worksheet_name;
-							}
-						);
-					}
-					// 日付に基づくフィルタリング
-					if ( strpos( $query, 'WHERE date >=' ) !== false ) {
-						$date = str_replace( array( "SELECT * FROM data WHERE date >= '", "'" ), '', $query );
-						return array_filter(
-							$mock_api_response,
-							function ( $item ) use ( $date ) {
-								return $item->date >= $date;
-							}
-						);
-					}
-					// 複数の条件に基づくフィルタリング
-					if ( strpos( $query, 'WHERE worksheet_name =' ) !== false && strpos( $query, 'AND date >=' ) !== false ) {
-						$worksheet_name = str_replace( array( "SELECT * FROM data WHERE worksheet_name = '", "' AND date >= '" ), '', explode( "' AND date >= '", $query )[0] );
-						$date           = str_replace( "'", '', explode( "' AND date >= '", $query )[1] );
-						return array_filter(
-							$mock_api_response,
-							function ( $item ) use ( $worksheet_name, $date ) {
-								return $item->worksheet_name === $worksheet_name && $item->date >= $date;
-							}
-						);
-					}
-					// 他の条件に基づくフィルタリングも同様に実装
-					return $mock_api_response;
-				}
-			)
 		);
 	}
 
 	/**
-	 * Tests retrieving rows starting from the 4th row in ascending order by ID.
+	 * Test getting all rows.
 	 */
-	public function testGetRowsFromFourth() {
-		$args                                 = array(
+	public function test_get_all_rows() {
+		$sheet = new Google_Spreadsheet_To_DB_Query();
+		$rows  = $sheet->getrow();
+
+		$this->assertCount( 4, $rows );
+	}
+
+	/**
+	 * Test getting 3 rows starting from the 4th row, ordered by ID in ascending order.
+	 */
+	public function test_get_3_rows_from_4th_ascending_by_id() {
+		$args  = array(
 			'orderby' => 'id',
 			'order'   => 'ASC',
 			'limit'   => 3,
 			'offset'  => 3,
 		);
-		$this->google_spreadsheet_to_db_query = new Google_Spreadsheet_To_DB_Query( $args );
-		$results                              = $this->google_spreadsheet_to_db_query->getrow();
-		$this->assertCount( 3, $results );
+		$sheet = new Google_Spreadsheet_To_DB_Query( $args );
+		$rows  = $sheet->getrow();
+
+		$this->assertCount( 1, $rows );
+		$this->assertEquals( 4, $rows[0]->id );
 	}
 
 	/**
-	 * Tests retrieving a row with a specific ID.
+	 * Test getting a row with a specific ID.
 	 */
-	public function testGetRowById() {
-		$args                                 = array(
+	public function test_get_row_with_specific_id() {
+		$args  = array(
 			'where' => array(
 				array(
 					'key'   => 'id',
-					'value' => 2,
+					'value' => 3,
 				),
 			),
 		);
-		$this->google_spreadsheet_to_db_query = new Google_Spreadsheet_To_DB_Query( $args );
-		$result                               = $this->google_spreadsheet_to_db_query->getrow();
-		$this->assertEquals( '2021-01-02', $result[0]->date );
-		$this->assertEquals( 'Sample Data 2', $result[0]->title );
+		$sheet = new Google_Spreadsheet_To_DB_Query( $args );
+		$rows  = $sheet->getrow();
+
+		$this->assertCount( 1, $rows );
+		$this->assertEquals( 3, $rows[0]->id );
 	}
 
-	/**
-	 * Tests retrieving rows from a specific worksheet sorted by ID.
-	 */
-	public function testGetRowsFromSpecificWorksheet() {
-		$args                                 = array(
+		/**
+		 * Test getting 3 rows with a specific worksheet name, ordered by ID.
+		 */
+	public function test_get_3_rows_with_specific_worksheet_ordered_by_id() {
+		$args  = array(
 			'orderby' => 'id',
 			'order'   => 'ASC',
 			'limit'   => 3,
 			'where'   => array(
 				array(
-					'key'     => 'worksheet_name',
-					'value'   => 'My Spreadsheet',
-					'compare' => '=',
+					'key'   => 'worksheet_name',
+					'value' => 'Sheet 1',
 				),
 			),
 		);
-		$this->google_spreadsheet_to_db_query = new Google_Spreadsheet_To_DB_Query( $args );
-		$results                              = $this->google_spreadsheet_to_db_query->getrow();
-		$this->assertCount( 3, $results );
+		$sheet = new Google_Spreadsheet_To_DB_Query( $args );
+		$rows  = $sheet->getrow();
+
+		$this->assertCount( 2, $rows );
+		$this->assertEquals( 1, $rows[0]->id );
+		$this->assertEquals( 2, $rows[1]->id );
 	}
 
 	/**
-	 * Tests retrieving rows with a date greater than a specified datetime.
+	 * Test getting rows with a specific sheet name.
 	 */
-	public function testGetRowsAfterDate() {
-		$args                                 = array(
+	public function test_get_rows_with_specific_sheet_name() {
+		$args  = array(
 			'where' => array(
 				array(
-					'key'     => 'date',
-					'value'   => '2024-01-01 00:00:00',
-					'compare' => '>=',
+					'key'   => 'sheet_name',
+					'value' => 'Data 3',
 				),
 			),
 		);
-		$this->google_spreadsheet_to_db_query = new Google_Spreadsheet_To_DB_Query( $args );
-		$results                              = $this->google_spreadsheet_to_db_query->getrow();
-		$this->assertNotEmpty( $results );
-		$this->assertCount( 3, $results );
+		$sheet = new Google_Spreadsheet_To_DB_Query( $args );
+		$rows  = $sheet->getrow();
+
+		$this->assertCount( 1, $rows );
+		$this->assertEquals( 'Data 3', $rows[0]->sheet_name );
 	}
 
 	/**
-	 * Tests retrieving rows based on multiple conditions.
+	 * Test getting rows with a specific title.
 	 */
-	public function testGetRowsWithMultipleConditions() {
-		$args                                 = array(
+	public function test_get_rows_with_specific_title() {
+		$args  = array(
 			'where' => array(
-				'relation' => 'AND',
 				array(
-					'key'     => 'date',
-					'value'   => '2024-01-01 00:00:00',
-					'compare' => '>=',
-				),
-				array(
-					'key'     => 'worksheet_name',
-					'value'   => 'My Spreadsheet',
-					'compare' => '=',
+					'key'   => 'title',
+					'value' => 'Title 4',
 				),
 			),
 		);
-		$this->google_spreadsheet_to_db_query = new Google_Spreadsheet_To_DB_Query( $args );
-		$results                              = $this->google_spreadsheet_to_db_query->getrow();
-		$this->assertNotEmpty( $results );
-		$this->assertCount( 2, $results );
+		$sheet = new Google_Spreadsheet_To_DB_Query( $args );
+		$rows  = $sheet->getrow();
+
+		$this->assertCount( 1, $rows );
+		$this->assertEquals( 'Title 4', $rows[0]->title );
 	}
 }
