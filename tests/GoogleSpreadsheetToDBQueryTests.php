c52af6bec44c287500fc37250c7b940c767e8f13
sect <sect@sect.me>
2024-04-29 02:02:44 +0900

    test: add some unit tests

--- tests/GoogleSpreadsheetToDBQueryTests.php
+++ tests/GoogleSpreadsheetToDBQueryTests.php
@@ -21,14 +21,40 @@ class Test_Google_Spreadsheet_To_DB_Query extends WP_UnitTestCase {
 		// Mock response from Google Sheets API.
 		$mock_api_response = array(
 			(object) array(
-				'id'    => 1,
-				'date'  => '2021-01-01',
-				'title' => 'Sample Data 1',
+				'id'             => 1,
+				'date'           => '2021-01-01',
+				'title'          => 'Sample Data 1',
+				'worksheet_name' => 'Sheet1',
 			),
 			(object) array(
-				'id'    => 2,
-				'date'  => '2021-01-02',
-				'title' => 'Sample Data 2',
+				'id'             => 2,
+				'date'           => '2021-01-02',
+				'title'          => 'Sample Data 2',
+				'worksheet_name' => 'Sheet1',
+			),
+			(object) array(
+				'id'             => 3,
+				'date'           => '2023-12-31',
+				'title'          => 'Sample Data 3',
+				'worksheet_name' => 'My Spreadsheet',
+			),
+			(object) array(
+				'id'             => 4,
+				'date'           => '2024-01-01',
+				'title'          => 'Sample Data 4',
+				'worksheet_name' => 'My Spreadsheet',
+			),
+			(object) array(
+				'id'             => 5,
+				'date'           => '2024-01-02',
+				'title'          => 'Sample Data 5',
+				'worksheet_name' => 'My Spreadsheet',
+			),
+			(object) array(
+				'id'             => 6,
+				'date'           => '2024-01-03',
+				'title'          => 'Sample Data 6',
+				'worksheet_name' => 'Sheet2',
 			),
 		);
 
@@ -37,17 +63,6 @@ class Test_Google_Spreadsheet_To_DB_Query extends WP_UnitTestCase {
 		$wpdb = $this->getMockBuilder( stdClass::class )
 					->setMethods( array( 'prepare', 'get_results', 'query' ) )
 					->getMock();
-
-		$wpdb->posts              = 'wp_posts';
-		$wpdb->postmeta           = 'wp_postmeta';
-		$wpdb->comments           = 'wp_comments';
-		$wpdb->commentmeta        = 'wp_commentmeta';
-		$wpdb->terms              = 'wp_terms';
-		$wpdb->term_taxonomy      = 'wp_term_taxonomy';
-		$wpdb->term_relationships = 'wp_term_relationships';
-		$wpdb->termmeta           = 'wp_termmeta';
-		$wpdb->users              = 'wp_users';
-		$wpdb->usermeta           = 'wp_usermeta';
 
 		$wpdb->method( 'prepare' )->will(
 			$this->returnCallback(
@@ -58,17 +73,106 @@ class Test_Google_Spreadsheet_To_DB_Query extends WP_UnitTestCase {
 		);
 		$wpdb->method( 'get_results' )->willReturn( $mock_api_response );
 
+		// Adjusting the instantiation to include parameters if needed.
 		$this->google_spreadsheet_to_db_query = new Google_Spreadsheet_To_DB_Query();
 	}
 
 	/**
-	 * Tests the getrow method of Google_Spreadsheet_To_DB_Query.
-	 * Ensures it returns the correct number of results and data.
+	 * Tests retrieving rows starting from the 4th row in ascending order by ID.
 	 */
-	public function testGetRow() {
-		$results = $this->google_spreadsheet_to_db_query->getrow();
+	public function testGetRowsFromFourth() {
+		$args                                 = array(
+			'orderby' => 'id',
+			'order'   => 'ASC',
+			'limit'   => 3,
+			'offset'  => 3,
+		);
+		$this->google_spreadsheet_to_db_query = new Google_Spreadsheet_To_DB_Query( $args );
+		$results                              = $this->google_spreadsheet_to_db_query->getrow();
+		$this->assertCount( 3, $results );
+	}
+
+	/**
+	 * Tests retrieving a row with a specific ID.
+	 */
+	public function testGetRowById() {
+		$args                                 = array(
+			'where' => array(
+				array(
+					'key'     => 'id',
+					'value'   => 2,
+					'compare' => '=',
+				),
+			),
+		);
+		$this->google_spreadsheet_to_db_query = new Google_Spreadsheet_To_DB_Query( $args );
+		$result                               = $this->google_spreadsheet_to_db_query->getrow();
+		$this->assertEquals( '2021-01-02', $result[0]->date );
+		$this->assertEquals( 'Sample Data 2', $result[0]->title );
+	}
+
+	/**
+	 * Tests retrieving rows from a specific worksheet sorted by ID.
+	 */
+	public function testGetRowsFromSpecificWorksheet() {
+		$args                                 = array(
+			'orderby' => 'id',
+			'order'   => 'ASC',
+			'limit'   => 3,
+			'where'   => array(
+				array(
+					'key'     => 'worksheet_name',
+					'value'   => 'My Spreadsheet',
+					'compare' => '=',
+				),
+			),
+		);
+		$this->google_spreadsheet_to_db_query = new Google_Spreadsheet_To_DB_Query( $args );
+		$results                              = $this->google_spreadsheet_to_db_query->getrow();
+		$this->assertCount( 3, $results );
+	}
+
+	/**
+	 * Tests retrieving rows with a date greater than a specified datetime.
+	 */
+	public function testGetRowsAfterDate() {
+		$args                                 = array(
+			'where' => array(
+				array(
+					'key'     => 'date',
+					'value'   => '2024-01-01 00:00:00',
+					'compare' => '>=',
+				),
+			),
+		);
+		$this->google_spreadsheet_to_db_query = new Google_Spreadsheet_To_DB_Query( $args );
+		$results                              = $this->google_spreadsheet_to_db_query->getrow();
+		$this->assertNotEmpty( $results );
+		$this->assertCount( 3, $results );
+	}
+
+	/**
+	 * Tests retrieving rows based on multiple conditions.
+	 */
+	public function testGetRowsWithMultipleConditions() {
+		$args                                 = array(
+			'where' => array(
+				'relation' => 'AND',
+				array(
+					'key'     => 'date',
+					'value'   => '2024-01-01 00:00:00',
+					'compare' => '>=',
+				),
+				array(
+					'key'     => 'worksheet_name',
+					'value'   => 'My Spreadsheet',
+					'compare' => '=',
+				),
+			),
+		);
+		$this->google_spreadsheet_to_db_query = new Google_Spreadsheet_To_DB_Query( $args );
+		$results                              = $this->google_spreadsheet_to_db_query->getrow();
+		$this->assertNotEmpty( $results );
 		$this->assertCount( 2, $results );
-		$this->assertEquals( '2021-01-01', $results[0]->date );
-		$this->assertEquals( 'Sample Data 1', $results[0]->title );
 	}
 }
