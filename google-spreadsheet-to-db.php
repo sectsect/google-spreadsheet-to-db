4d0177a9b69665d0fdab43c55ab0a84bd1877739
sect <sect@sect.me>
2024-04-29 00:35:54 +0900

    chore: bump version v6.1.0

--- google-spreadsheet-to-db.php
+++ google-spreadsheet-to-db.php
@@ -7,7 +7,7 @@
  * Author URI:      https://github.com/sectsect/google-spreadsheet-to-db
  * Text Domain:     google-spreadsheet-to-db
  * Domain Path:     /languages
- * Version:         6.0.0
+ * Version:         6.1.0
  *
  * @package         Google_Spreadsheet_to_DB
  */
